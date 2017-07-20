2d6ed78c3548a349eb3edee42e0a364d4c944646
zezemanolo <manoloish@gmail.com>
2017-07-19 21:18:11 -0700

    adds search results list to mmaps

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
-let map, infowindow, service;
-let searchResults = [];
+let map, infowindow, service
+let searchResults = []
 const center = new google.maps.LatLng(37.7853448, -122.3975658)
+let counter = 1
 const request = {
   location: center,
   radius: 8700,
@@ -21,78 +22,86 @@ const mapControls = {
   scaleControl: true,
   streetViewControl: true,
   streetViewControlOptions: {
-      position: google.maps.ControlPosition.LEFT_TOP
+      position: google.maps.ControlPosition.TOP_LEFT
   },
   fullscreenControl: true
 }
 
 function initMap() {
-  const input = document.getElementById('pac-input');
-  const searchBox = new google.maps.places.SearchBox(input);
-
   map = new google.maps.Map(document.getElementById('map'), mapControls)
   service = new google.maps.places.PlacesService(map)
   infowindow = new google.maps.InfoWindow()
-  service.nearbySearch(request, callback)
+  service.nearbySearch(request, initialSearch)
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  const input = document.getElementById('search-input')
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input)
+
+  const trafficLayer = new google.maps.TrafficLayer()
+  trafficLayer.setMap(map)
+
+  const searchBox = new google.maps.places.SearchBox(input)
 
   map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
+    searchBox.setBounds(map.getBounds())
+  })
   searchBox.addListener('places_changed', function() {
-  const places = searchBox.getPlaces();
+  const places = searchBox.getPlaces()
 
   if (places.length == 0) {
-    return;
+    return
   }
 
   searchResults.forEach(function(marker) {
-    marker.setMap(null);
-  });
-  const bounds = new google.maps.LatLngBounds();
+    marker.setMap(null)
+  })
+  const bounds = new google.maps.LatLngBounds()
 
   places.forEach(function(place) {
     if (!place.geometry) {
-      console.log("Returned place contains no geometry");
-      return;
+      console.log("Returned place contains no geometry")
+      return
     }
-    let searchItem = {
+    let searchIcon = {
       url: place.icon,
       size: new google.maps.Size(71, 71),
       origin: new google.maps.Point(0, 0),
       anchor: new google.maps.Point(17, 34),
       scaledSize: new google.maps.Size(25, 25)
-    };
+    }
 
     searchResults.push(new google.maps.Marker({
       map: map,
-      icon: searchItem,
+      icon: searchIcon,
       title: place.name,
       position: place.geometry.location
-    }));
+    }))
 
     if (place.geometry.viewport) {
-      bounds.union(place.geometry.viewport);
+      bounds.union(place.geometry.viewport)
     } else {
-      bounds.extend(place.geometry.location);
+      bounds.extend(place.geometry.location)
     }
     google.maps.event.addListener(searchResults[0], 'click', function() {
-      infowindow.setContent('<div><strong>' + place.name + '</strong><br><i>' + place.rating + ' star rating!</i>' + '<br>' + place.vicinity + '</div>');
-      infowindow.open(map, this);
-    });
-  });
-  map.fitBounds(bounds);
-  });
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br><i>' + place.rating + ' star rating!</i>' + '<br>' + place.vicinity + '</div>')
+      infowindow.open(map, this)
+    })
+  })
+  map.fitBounds(bounds)
+  })
 
 }
 
-function callback(results, status) {
+function initialSearch(results, status) {
+  let bounds = new google.maps.LatLngBounds
+  let placesList = document.getElementById('places')
+
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (let i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+      createMarker(results[i])
+      placesList.innerHTML += '<li>' + results[i].name + '</li>'
+      bounds.extend(results[i].geometry.location)
     }
+    map.fitBounds(bounds)
   }
 }
 
@@ -101,19 +110,19 @@ function createMarker(place, timeout) {
     map: map,
     title: place.name,
     animation:google.maps.Animation.DROP,
-    // icon: typeof place.photos !== 'undefined' ? place.photos[0].getUrl({'maxWidth': 65, 'maxHeight': 65}) : '',
-    //cool feature, but it looks janky
     place: {
       placeId: place.place_id,
       location: place.geometry.location
     }
   }
-  marker = new google.maps.Marker(markerSpecs);
+
+  marker = new google.maps.Marker(markerSpecs)
 
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br><i>' + place.rating + ' star rating!</i>' + '<br>' + place.vicinity + '</div>');
-    infowindow.open(map, this);
-  });
-  console.log(place);
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br><i>' + place.rating + ' star rating!</i>' + '<br>' + place.vicinity + '</div>')
+    infowindow.open(map, this)
+  })
+  console.log(place)
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+
+google.maps.event.addDomListener(window, 'load', initMap)
